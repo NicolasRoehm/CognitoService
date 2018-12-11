fed6cb2c8aabdfacc0e7b857d5b70a2036cfd08b
Nicolas Roehm <nicolas.roehm@gmail.com>
2018-12-11 14:30:14 +0100

    [Fix] import

--- projects/cognito-service/src/public_api.ts
+++ projects/cognito-service/src/public_api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of cognito-service
  */
 
-export * from './lib/cognito.service';
 export * from './lib/models/cognito-service-response.model';
 export * from './lib/enums/auth-type.enum';
 export * from './lib/enums/resp-type.enum';
+export * from './lib/cognito.service';
